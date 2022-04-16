4f168e94aaa72d1f7db0227ea3494f776cc7bc35
Christoph Eicke <christoph@geisterstunde.org>
2022-04-16 13:44:09 +0200

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,10 @@ set :branch, "main"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
 
+# deploy.rb
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_files, 'config/database.yml', 'config/master.key', 'config/credentials/production.key'
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
