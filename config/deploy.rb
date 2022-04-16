fb67861dfabc3a84e727f59612746db866adb603
Christoph Eicke <christoph@geisterstunde.org>
2022-04-16 14:24:55 +0200

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,21 +44,11 @@ append :linked_files, 'config/master.key', 'config/credentials/production.key'
 # set :ssh_options, verify_host_key: :secure
 
 set :user, 'photastic2'
-set :puma_threads, [4, 16]
-set :puma_workers, 0
 
 set :pty, true
 set :use_sudo, false
 set :stage, :production
 set :deploy_via, :remote_cache
 set :deploy_to, "/home/#{fetch(:user)}/app/"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log, "#{release_path}/log/puma.error.log"
 set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :rvm_ruby_verion, 'ruby-3.1.0'
